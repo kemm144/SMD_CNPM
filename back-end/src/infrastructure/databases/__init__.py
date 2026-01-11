c7480591bafe0e35eb1272ce08dd8c3df5a2399d
Hiền-Nhân <huynhvohiennhan7@gmail.com>
2026-01-11 21:43:48 +0700

    reset

--- back-end/src/infrastructure/databases/__init__.py
+++ back-end/src/infrastructure/databases/__init__.py
@@ -1,5 +1,11 @@
 from infrastructure.databases.mssql import init_mssql
-from infrastructure.models import todo_model, user_model, faculties_model, academicYears_model, assessmentSchemes_model, syllabusComments_model, syllabuses_model, workflowLogs_model 
+from infrastructure.models import (
+    todo_model, 
+    user_model, 
+    faculties_model, 
+    academicYears_model, 
+    assessmentSchemes_model
+)
 
 def init_db(app):
     init_mssql(app)
